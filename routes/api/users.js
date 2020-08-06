34efca4b87d4617af9eee197fbd8999b9b02d557
davidsdevel <djgm1206@gmail.com>
2020-08-05 22:23:24 -0430

    Added Local Changes

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,6 +1,23 @@
 const router = require('express').Router();
 
 router
+  .get("/v/:emailHex", async ({query, params, db}, res) => {
+    const {key} = query;
+    const {emailHex} = params;
+
+    try {
+      const userVerified = await db.verifyUser(Buffer.from(emailHex, "hex").toString("utf-8"), key);
+
+      if (userVerified) {
+        res.redirect(302, "/");
+      } else {
+        res.status(404);
+      }
+      //TODO: Implement Redirects or Render page on successfully or on error verification
+    } catch(err) {
+      res.status(500).send(err.toString());
+    }
+  })
   .get('/:action', async (req, res) => {
     try {
       const { action } = req.params;
